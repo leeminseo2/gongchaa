b179367cdbea933901817e2e6a1750314b7bebbc
leeminseo <lms316497@naver.com>
2024-07-06 12:38:33 +0900

    gongcha

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(function () {
+  // Initialize the work slider
+  gsap.registerPlugin(ScrollTrigger);
+
+  const bestSlider = new Swiper('.best-slider', {
+    loop: true,
+    slidesPerView: 3,
+    // spaceBetween: 50,
+
+    centeredSlides: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+  });
+  const newSlider = new Swiper('.new-slider', {
+    loop: true,
+
+    spaceBetween: 20,
+    slidesPerView: 3,
+    // centeredSlides: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.new-button-next',
+      prevEl: '.new-button-prev',
+    },
+  });
+});
